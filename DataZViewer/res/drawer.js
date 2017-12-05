61d2de2ce1162bbe14a0313a319ca50e577ee52a
Dan Waxman <dan.waxman1@gmail.com>
2017-12-04 21:14:26 -0500

    Added multifile support
    Added multifile support for drawing using HTML5 Drawing API

--- DataZViewer/res/drawer.js
+++ DataZViewer/res/drawer.js
@@ -1,5 +1,5 @@
-var x = [];
-var y = [];
+var x = new Array();
+var y = new Array();
 var basetime = 100;
 var pauseIndex = 0;
 var pause = false;
@@ -10,22 +10,27 @@ function handleFileSelect(evt) {
   var output = [];
   for (var i = 0, f; f = files[i]; i++) {
     var reader = new FileReader();
-
     reader.readAsText(f);
 
     reader.onload = function(e) {
-      parseCSV(reader.result);
+      if (e.target.readyState == FileReader.DONE) {
+        parseCSV(e.target.result);
+      }
     };
   }
 }
 
 function parseCSV(csv_input) {
   var rows = csv_input.split("\n");
+  var x_temp = new Array();
+  var y_temp = new Array();
   for (j = 1; j < rows.length; j++) {
     var row_values = rows[j].split(",");
-    x.push(row_values[0]);
-    y.push(row_values[1]);
+    x_temp.push(row_values[0]);
+    y_temp.push(row_values[1]);
   }
+  x.push(x_temp);
+  y.push(y_temp);
 }
 
 function init() {
@@ -65,11 +70,12 @@ function draw(i) {
     ctx.clearRect(0, 0, 1000, 500);
     ctx.beginPath();
   }
-
-  if (i < x.length - 1) {
+  if (i < x[0].length - 1) {
     setTimeout(function() {
-      ctx.moveTo(1000.0 / 54.0 * x[i], 500 - (500.0 / 27.0 * y[i]));
-      ctx.lineTo(1000.0 / 54.0 * x[i + 1], 500 - (500.0 / 27.0 * y[i + 1]));
+      for (j = 0; j < x.length; j++) {
+        ctx.moveTo(1000.0 / 54.0 * x[j][i], 500 - (500.0 / 27.0 * y[j][i]));
+        ctx.lineTo(1000.0 / 54.0 * x[j][i + 1], 500 - (500.0 / 27.0 * y[j][i + 1]));
+      }
 
       ctx.stroke()
       draw(i + 1);
