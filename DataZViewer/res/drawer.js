a906ec6131f61ccc60fd2e2d30e7868caffb2108
Dan Waxman <dan.waxman1@gmail.com>
2017-12-04 20:49:02 -0500

    Added Single CSV Support
    Added support for a single CSV to be read and processed.

--- DataZViewer/res/drawer.js
+++ DataZViewer/res/drawer.js
@@ -4,48 +4,66 @@ var basetime = 100;
 var pauseIndex = 0;
 var pause = false;
 
+function handleFileSelect(evt) {
+  var files = evt.target.files;
+
+  var output = [];
+  for (var i = 0, f; f = files[i]; i++) {
+    var reader = new FileReader();
+
+    reader.readAsText(f);
+
+    reader.onload = function(e) {
+      parseCSV(reader.result);
+    };
+  }
+}
+
+function parseCSV(csv_input) {
+  var rows = csv_input.split("\n");
+  for (j = 1; j < rows.length; j++) {
+    var row_values = rows[j].split(",");
+    x.push(row_values[0]);
+    y.push(row_values[1]);
+  }
+}
+
 function init() {
-document.getElementById('pause-button').onclick = function() {
-		pause = true;
+  document.getElementById('pause-button').onclick = function() {
+    pause = true;
   };
-  
+
   document.getElementById('play-button').onclick = function() {
-		if (pause) {
-    	pause = false;
-    	draw(pauseIndex);
+    if (pause) {
+      pause = false;
+      draw(pauseIndex);
     }
   };
 
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
   document.getElementById('initiator').onclick = function() {
-  pause = false;
-  	var x_t = document.getElementById('xval').value.split(',');
-    var y_t = document.getElementById('yval').value.split(',');
-    x = []
-    y = []
- for (i = 0; i < x_t.length; i++) {
-    x[i] = parseFloat(x_t[i]);
-    y[i] = parseFloat(y_t[i]);
-  }
+    pause = false;
     draw(0);
   };
 }
 
 function slider() {
-	return (1000 - document.getElementById('speed').value) / 500 ;
+  return (1000 - document.getElementById('speed').value) / 500;
 }
 
 function draw(i) {
-	if (pause) {
-  	pauseIndex = i;
+  if (pause) {
+    pauseIndex = i;
     return;
   }
-  
- 
+
+
   var ctx = document.getElementById('canvas').getContext('2d');
 
- if (i == 0) {
-  ctx.clearRect(0, 0, 1000, 500);
-  ctx.beginPath();
+  if (i == 0) {
+    ctx.clearRect(0, 0, 1000, 500);
+    ctx.beginPath();
   }
 
   if (i < x.length - 1) {
@@ -60,4 +78,3 @@ function draw(i) {
 }
 
 init();
-
