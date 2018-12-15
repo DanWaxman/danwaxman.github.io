0d3e8f9ffc733f18d53d0f69a0d343dd4bb1301f
Dan Waxman <dan.waxman1@gmail.com>
2018-12-14 19:15:10 -0500

    Moved to OpenCV.js and added some preliminary Canny edge detection.

--- APictureIs1000Words/res/script.js
+++ APictureIs1000Words/res/script.js
@@ -1,30 +1,22 @@
-function init() {
-    var canvas = document.getElementById('color-image');
+$( document ).ready(function() {
+    // Load OpenCV Library
+    $.getScript( '/res/opencv.js', function( data, textStatus, jqxhr ) {
+        console.log( 'OpenCV.js load was performed | Status: ' + textStatus + ' ' + jqxhr.status);
+    });
 
-    var img = new Image;
-    img.src = 'res/coloredpencils.jpg'
-    img.onload = function() {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
-        color2bw(canvas);
-    }
+    // Set the input image to an uploaded one when the upload changes
+    $( '#fileInput' ).on( 'change', function(e) {
+        $( '#imageSrc' ).attr('src', URL.createObjectURL(e.target.files[0]));
+    });
 
-}
+    // After the input image changes, do some image manipulation
+    $( '#imageSrc').on( 'load', function() {
+        let src = cv.imread('imageSrc');
+        let dst = new cv.Mat();
+        cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
 
-function color2bw(imgCanvas) {
-    var ctx = imgCanvas.getContext('2d');
-    var pixelData = ctx.getImageData(0, 0, imgCanvas.width, imgCanvas.height);
-    var data = pixelData.data;
-
-    for (var i = 0; i < data.length; i += 4) {
-        var lum = 0.21 * data[i] + 0.72 * data[i + 1] + 0.07 * data[i + 2];
-        data[i] = lum;
-        data[i + 1] = lum;
-        data[i + 2] = lum;
-    }
-
-    ctx.putImageData(pixelData, 0, 0);
-}
-
-window.onload = init();
+        cv.Canny(src, dst, 50, 100, 3, false);
+        cv.imshow('canvasOutput', dst);
+        src.delete(); dst.delete();
+    });
+});
